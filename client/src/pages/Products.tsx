4d23a6d00ffcb0bcb46be8baca5b1f57eeefc7ec
OdyLiagkas <31992887+OdyLiagkas@users.noreply.github.com>
2025-12-31 10:40:51 -0500

    Update Products.tsx

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -9,7 +9,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "wouter";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardContent } from "@/components/ui/card";
-import productImage from "@assets/Period_products_collection_photo_196a839e-VwUNoBus.png";
+import productImage from "@/Period_products_collection_photo_196a839e.png";
 
 
 const categories = ["All", "Tampons", "Pads", "Cups", "Underwear", "Discs", "Liners"];
