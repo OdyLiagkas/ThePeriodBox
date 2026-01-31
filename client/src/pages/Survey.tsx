37954d9354d279a531a85c2e28884109cc7b4066
Odyssefs Liagkas <odyssefs@Mac.lan>
2026-01-31 01:01:42 -0500

    back:

--- client/src/pages/Survey.tsx
+++ client/src/pages/Survey.tsx
@@ -240,11 +240,9 @@ const res = await fetch("/api/survey-responses", {
   method: "POST",
   credentials: "include",
   headers: { "Content-Type": "application/json" },
-body: JSON.stringify({
-  sessionId,
-  answers: surveyData.answers,
-}),
-
+  body: JSON.stringify({ answers: surveyData.answers,
+                        userId: user.id,
+   }),
 });
 
 =======
