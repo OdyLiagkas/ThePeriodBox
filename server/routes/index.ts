7a6c64eeeb1a3d37b9cb152a48e9714bc51277ea
Odyssefs Liagkas <odyssefs@Mac.lan>
2026-02-16 04:06:10 -0500

    fixed routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,10 @@
 // server/routes/index.ts
 import express from "express";
-import surveyRouter from "./survey"; // <-- imports the "router" from survey.ts
+import surveyRouter from "./survey";
+import usersRouter from "./users"; 
 
 export function registerRoutes(app: express.Express) {
-  app.use("/api", surveyRouter); // mounts all routes in survey.ts under /api
+  app.use("/api", surveyRouter);
+  app.use("/api", usersRouter);
   return app;
 }
